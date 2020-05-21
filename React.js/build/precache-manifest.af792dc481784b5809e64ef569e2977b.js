aa67841c08de8b76a1615568c4f3d92d6405ac09
Janek <radziminski.j@gmail.com>
2020-05-21 12:40:52 +0200

    Third project-  react

---
+++ React.js/build/precache-manifest.af792dc481784b5809e64ef569e2977b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e19bedeed1037b58eb3afeb8c869cf48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79b460de95f06495cda2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4ab6cb875f0803ae7314",
+    "url": "/static/js/2.e2c9b9c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e2c9b9c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79b460de95f06495cda2",
+    "url": "/static/js/main.6ff0a6c4.chunk.js"
+  },
+  {
+    "revision": "72c08405e1cf2e1dbca9",
+    "url": "/static/js/runtime-main.724e940c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ React.js/src/views/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Calendar from '../components/Calendar';
+
+const Home = () => {
+    return (
+        <section className="section-home">
+            <Calendar />
+        </section>
+    );
+};
+
+export default Home;
